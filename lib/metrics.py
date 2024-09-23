6e0c1ed2b4c85d0123445991e79acb3a99587f72
henry <hmarichal93@gmail.com>
2024-09-23 10:38:33 -0300

    plantetion tree date or harvest date

--- lib/metrics.py
+++ lib/metrics.py
@@ -20,8 +20,8 @@ from lib.image import Color, Drawing
 from backend.labelme_layer import AL_AnnualRings
 
 
-def export_results(labelme_latewood_path : str, labelme_earlywood_path : str, image_path : str, metadata: dict,
-                   output_path="output/measures.csv", draw=True):
+def export_results( labelme_latewood_path : str, labelme_earlywood_path : str, image_path : str, metadata: dict,
+                   output_path="output/measures.csv", draw=True, plantation_date=True):
     #metadata
     year = metadata["year"]
     year = datetime.datetime(year, 1, 1)
@@ -45,13 +45,13 @@ def export_results(labelme_latewood_path : str, labelme_earlywood_path : str, im
         area = ring.area
         latewood_area = ring.late_wood.area if ring.late_wood is not None else 0
         earlywood_area = ring.early_wood.area if ring.early_wood is not None else 0
-        area_latewood_earlywood = latewood_area / earlywood_area if latewood_area > 0 and earlywood_area > 0 else 0
+        area_latewood_earlywood = None #latewood_area / earlywood_area if latewood_area > 0 and earlywood_area > 0 else 0
 
         #width
-        width_annual_ring = ring.equivalent_radii()
-        width_latewood = ring.late_wood.equivalent_radii() if ring.late_wood is not None else 0
-        width_earlywood = ring.early_wood.equivalent_radii() if ring.early_wood is not None else 0
-        width_latewood_earlywood = width_latewood / width_earlywood if width_latewood > 0 and width_earlywood > 0 else 0
+        width_annual_ring = None #ring.equivalent_radii()
+        width_latewood = None #ring.late_wood.equivalent_radii() if ring.late_wood is not None else 0
+        width_earlywood = None #ring.early_wood.equivalent_radii() if ring.early_wood is not None else 0
+        width_latewood_earlywood = None #width_latewood / width_earlywood if width_latewood > 0 and width_earlywood > 0 else 0
 
         #eccentricity
         if idx == 0:
@@ -69,14 +69,14 @@ def export_results(labelme_latewood_path : str, labelme_earlywood_path : str, im
         df.loc[idx] = [
             f"{ring.main_label}", f"{ring.secondary_label}", year.year,
             area * (pixels_millimeter_relation**2), earlywood_area * (pixels_millimeter_relation**2),
-            latewood_area * (pixels_millimeter_relation**2) , area_latewood_earlywood * (pixels_millimeter_relation**2),
+            latewood_area * (pixels_millimeter_relation**2) , area_latewood_earlywood * (pixels_millimeter_relation),
             width_annual_ring * pixels_millimeter_relation, width_earlywood * pixels_millimeter_relation ,
             width_latewood * pixels_millimeter_relation , width_latewood_earlywood * pixels_millimeter_relation,
             eccentricity_module * pixels_millimeter_relation, eccentricity_phase, ring_similarity_factor
         ]
         image_full = ring.draw_rings(image_full, thickness=3)
         #increase year by 1 year more
-        year = year + datetime.timedelta(days=365)
+        year = year + datetime.timedelta(days=365) if plantation_date else year - datetime.timedelta(days=365)
         if draw:
             thickness= 3
             image_debug = ring.draw(image.copy(), full_details=True, opacity=0.1)
