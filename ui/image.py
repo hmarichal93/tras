c1f5e42afd44d2ad4ea3df23724dd0ef627e28dc
henry <hmarichal93@gmail.com>
2024-10-14 10:38:57 -0300

    added u2net

--- ui/image.py
+++ ui/image.py
@@ -8,8 +8,7 @@ from PIL import Image
 from streamlit_option_menu import option_menu
 from pathlib import Path
 
-from lib.image import resize_image_using_pil_lib, load_image, write_image
-from lib.io import load_json
+from lib.image import resize_image_using_pil_lib, load_image, write_image, remove_salient_object
 from ui.common import Context, RunningWidget
 from backend.labelme_layer import (LabelmeShapeType,
                                    LabelmeObject, LabelmeInterface as UserInterface, resize_annotations)
@@ -165,7 +164,7 @@ def main(runtime_config_path):
 
 
     if selected == Menu.preprocess and Path(CTX.image_path).exists():
-        if st.button("Remove Background"):
+        if st.button("Manually Remove Background"):
             gif_runner = RunningWidget()
             interface = BackgroundInterface(CTX.image_path, CTX.background_json_path, CTX.image_no_background_path)
             interface.interface()
@@ -174,6 +173,11 @@ def main(runtime_config_path):
                 CTX.bg_image_pil_no_background = interface.remove_background()
                 CTX.bg_image_pil_no_background = CTX.bg_image_pil_no_background.resize((CTX.display_image_size,
                                                                 CTX.display_image_size), Image.Resampling.LANCZOS)
+
+            gif_runner.empty()
+        if st.button("Automatic Remove background"):
+            gif_runner = RunningWidget()
+            remove_salient_object(CTX.image_path, CTX.image_no_background_path)
 
             gif_runner.empty()
 
