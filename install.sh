f8a60f276c9fd51000c0ee96d964328f106f6172
henry <hmarichal93@gmail.com>
2025-04-17 12:17:07 -0300

    deepcstrd installation steps

--- install.sh
+++ install.sh
@@ -56,6 +56,20 @@ else
     exit 1
 fi
 
+# Install DeepCSTRD project dependencies
+echo "Installing DeepCSTRD dependencies..."
+DEEPCSTRD_DIR="$ROOT_DIRECTORY/automatic_methods/tree_ring_delineation/deepcstrd"
+cd $DEEPCSTRD_DIR || { echo "Directory not found: $DEEPCSTRD_DIR"; exit 1; }
+if git submodule update --init && git lfs install && git lfs pull && python setup.py install && pip install -r requirements.txt; then
+    git clone git@github.com:hmarichal93/cstrd.git && cd cstrd && python setup.py install
+    cd ../
+    git clone https://github.com/hmarichal93/uruDendro.git && cd uruDendro && python setup.py install
+    echo "DeepCSTRD dependencies installed successfully."
+else
+    echo "Error installing DeepCSTRD dependencies."
+    exit 1
+fi
+
 # Installing APD project
 
 
